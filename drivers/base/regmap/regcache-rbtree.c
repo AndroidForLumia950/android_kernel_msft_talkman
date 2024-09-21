41879dc44717a015b8a3cba33db26391984524b2
djokcho <probgdjokcho@gmail.com>
2024-09-21 20:20:54 +0300

    fix a ton of warnings

--- drivers/base/regmap/regcache-rbtree.c
+++ drivers/base/regmap/regcache-rbtree.c
@@ -31,7 +31,7 @@ struct regcache_rbtree_node {
 	void *block;
 	/* number of registers available in the block */
 	unsigned int blklen;
-} __attribute__ ((packed));
+} __attribute__((packed, aligned(8)));
 
 struct regcache_rbtree_ctx {
 	struct rb_root root;
