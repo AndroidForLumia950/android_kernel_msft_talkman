c961c07079367014b8f48a28f4069bd68f8045d7
djokcho <probgdjokcho@gmail.com>
2024-10-27 23:08:03 +0200

    Revert "Upstream decompress"
    This reverts commit e003707e3a2c0f603da9ad62f491f72241581db7.

--- include/uapi/linux/input.h
+++ include/uapi/linux/input.h
@@ -156,9 +156,6 @@ struct input_keymap_entry {
 #define EVIOCGSUSPENDBLOCK	_IOR('E', 0x91, int)			/* get suspend block enable */
 #define EVIOCSSUSPENDBLOCK	_IOW('E', 0x91, int)			/* set suspend block enable */
 
-#define EVIOCGSUSPENDBLOCK	_IOR('E', 0x91, int)			/* get suspend block enable */
-#define EVIOCSSUSPENDBLOCK	_IOW('E', 0x91, int)			/* set suspend block enable */
-
 #define EVIOCSCLOCKID		_IOW('E', 0xa0, int)			/* Set clockid to be used for timestamps */
 
 /*
