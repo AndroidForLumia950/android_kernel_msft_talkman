9ad9316df4b10ab06f508a7cb56b3c532a9c88fd
djokcho <140524342+djokcho@users.noreply.github.com>
2024-11-03 15:55:58 +0200

    Update tas2552.c

--- sound/soc/codecs/tas2552.c
+++ sound/soc/codecs/tas2552.c
@@ -16,6 +16,7 @@
 #include <linux/i2c.h>
 #include <linux/module.h>
 #include <linux/slab.h>
+#include <linux/of_device.h>
 #include <linux/gpio.h>
 #include <linux/of_gpio.h>
 #include <linux/delay.h>
